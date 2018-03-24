9e4dd0685baad95800f46405e13ed27668481a7a
etan-bard <etan.bard@gmail.com>
2018-03-24 14:57:30 -0700

    Trying to fix issue with unique exceeding the max limit in DB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 191);
+            $table->unique('email', 'unique_email');
             $table->string('password');
             $table->integer('is_admin')->unsigned()->default(0);
             $table->rememberToken();
